02e1cbfd834f14b36474fc9d2694615b8a87d2f6
SLiezzan <sliezan19@gmail.com>
2017-02-18 13:38:11 +0100

    Estado actual: Ejercicio4 P1 pendiente

--- 01_P1/ejercicio4.1.c
+++ 01_P1/ejercicio4.1.c
@@ -3,12 +3,15 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <string.h>
 
 #define NUM_PROC 3
+#define MAX_CHAR 128
 
 int main (void){
-  pid_t pid;
-  int i;
+  pid_t pid, aux_son;
+  int i, exe_status;
+  char pid_c[MAX_CHAR];
 
   for(i=0; i<NUM_PROC; i++) {
     if((pid=fork())<0) {
@@ -17,10 +20,23 @@ int main (void){
     } else if (pid==0) {
       printf("HIJO %d, PID: %d, PPID: %d\n", i, getpid(), getppid());
     } else{
-      printf("PADRE %d\n", i);
+      if((pid=fork())<0) {
+        exit(EXIT_FAILURE);
+      } else if (pid==0) {
+        sprintf(pid_c, "%d", getppid());
+        char *buf[] = {"pstree", pid_c, "-p", NULL};
+        fprintf(stdout, "%s %s %s %d\n", buf[0], buf[1], buf[2], getpid());
+        aux_son = getpid();
+        if ((exe_status = execvp("pstree", buf)) == -1) {
+          printf("Entras aquí alguna vez?\n");
+          exit(EXIT_FAILURE);
+        }
+      } else {
+        waitpid(aux_son, NULL, 0);
+      }
+      printf("PADRE %d, PID: %d, PPID: %d\n", i, getpid(), getppid());
     }
   }
 
-  while(1);
   exit(EXIT_SUCCESS);
 }
