06b73b97f5b7d51ab9bafad2d24df72478d6e9f4
blancams <blancamartinselgas@gmail.com>
2017-02-18 15:01:32 +0100

    Problemas con el commit...

--- 01_P1/ejercicio4.1.c
+++ 01_P1/ejercicio4.1.c
@@ -9,13 +9,8 @@
 #define MAX_CHAR 128
 
 int main (void){
-<<<<<<< HEAD
   pid_t pid;
   int i;
-=======
-  pid_t pid, aux_son;
-  int i, exe_status;
->>>>>>> 02e1cbfd834f14b36474fc9d2694615b8a87d2f6
   char pid_c[MAX_CHAR];
 
   for(i = 0 ; i<NUM_PROC; i++) {
@@ -25,17 +20,12 @@ int main (void){
     } else if (pid==0) {
       printf("HIJO %d, PID: %d, PPID: %d\n", i, getpid(), getppid());
     } else{
-<<<<<<< HEAD
       printf("PADRE %d, PID: %d, PPID: %d\n", i, getpid(), getppid());
-
-=======
->>>>>>> 02e1cbfd834f14b36474fc9d2694615b8a87d2f6
       if((pid=fork())<0) {
         exit(EXIT_FAILURE);
       } else if (pid==0) {
         sprintf(pid_c, "%d", getppid());
         char *buf[] = {"pstree", pid_c, "-p", NULL};
-<<<<<<< HEAD
         //fprintf(stdout, "%s %s %s %d\n", buf[0], buf[1], buf[2], getpid());        
         if (execvp("pstree", buf) == -1) {
           printf("Entras aquí alguna vez?\n");
@@ -44,18 +34,6 @@ int main (void){
       } else{
         waitpid(pid, NULL, 0);
       } 
-=======
-        fprintf(stdout, "%s %s %s %d\n", buf[0], buf[1], buf[2], getpid());
-        aux_son = getpid();
-        if ((exe_status = execvp("pstree", buf)) == -1) {
-          printf("Entras aquí alguna vez?\n");
-          exit(EXIT_FAILURE);
-        }
-      } else {
-        waitpid(aux_son, NULL, 0);
-      }
-      printf("PADRE %d, PID: %d, PPID: %d\n", i, getpid(), getppid());
->>>>>>> 02e1cbfd834f14b36474fc9d2694615b8a87d2f6
     }
   }
 
