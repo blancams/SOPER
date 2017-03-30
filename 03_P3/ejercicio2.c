5ddb19a3d8ad8587c27eae2057823f41fac0dd88
blancams <blancamartinselgas@gmail.com>
2017-03-30 14:43:03 +0200

    Ejercicios 2 y 5

---
+++ 03_P3/ejercicio2.c
@@ -0,0 +1,106 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h> 
+#include <sys/ipc.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/shm.h> 
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+#define MAX_CHAR 80
+#define FILEKEY "/bin/cat" 
+#define KEY 1301
+
+struct info{ 
+   char nombre[MAX_CHAR]; 
+   int id; 
+};
+
+void manejador(int sig);
+int num_aleatorio(int inf, int sup);
+
+int main(int argc, char *argv[]){
+   int key, id;
+   int i, n;
+   int pid;
+   struct info *mens;
+
+   if(argc != 2){
+      printf("Fallo en los argumentos de entrada.\n");
+      exit(EXIT_FAILURE);
+   }
+   n = atoi(argv[1]);
+
+   key = ftok(FILEKEY, KEY);
+   if(key == -1){
+      printf("Error al generar la clave.\n");
+      exit(EXIT_FAILURE);
+   }
+
+   id = shmget(key, sizeof(mens), IPC_CREAT | IPC_EXCL | SHM_R | SHM_W);
+   if(id == -1){
+      printf("Error en la reserva del bloque de memoria.\n");
+      exit(EXIT_FAILURE);
+   }
+
+   if(signal(SIGUSR1, manejador) == SIG_ERR){
+      printf("Error en la asignacion del manejador.\n");
+      exit(EXIT_FAILURE);
+   }
+   
+   for(i = 0; i < n; i++){
+      if((pid = fork()) == -1){
+         printf("Error al hacer el fork.\n");
+         exit(EXIT_FAILURE);
+      } else if(!pid){
+         break;
+      } else{
+         ;
+      }
+   }
+
+   if((mens = (struct info *) shmat(id, NULL, 0)) == NULL){
+      printf("Error en el acceso a la memoria compartida.\n");
+      shmdt((char *) mens);
+      exit(EXIT_FAILURE);
+   }
+
+   if(pid){
+      for(i = 0; i < n; i++){
+         if(pause() != -1){
+            printf("Error al hacer el pause.\n");
+            exit(EXIT_FAILURE);
+         }
+         printf("Nombre de usuario: %s\tID: %d\n", mens->nombre, mens->id);
+         fflush(stdout);
+      }
+      for(i = 0; i < n; i++){
+         wait(NULL);
+      }
+      shmdt((char *) mens);
+      shmctl(id, IPC_RMID, (struct shmid_ds *) NULL);
+   } else if(!pid){
+      sleep(num_aleatorio(i,10));
+      printf("\nIntroduzca el nombre de un cliente: \n");
+      scanf("%s", mens->nombre);
+      mens->id++;
+      shmdt((char *) mens);
+      if(kill(getppid(), SIGUSR1) == -1){
+         printf("Error en el envio de señal SIGUSR1.\n");
+         exit(EXIT_FAILURE);
+      }
+      
+   }
+   exit(EXIT_SUCCESS);
+}
+
+void manejador(int sig){
+   return;
+}
+
+int num_aleatorio(int inf, int sup){
+   return rand() % (sup - inf + 1) + inf;
+}
---
+++ 03_P3/semaforos.c
@@ -0,0 +1,93 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/types.h>
+#include <errno.h> 
+#include <sys/shm.h> 
+
+#include "semaforos.h"
+
+union semun { 
+	int val; 
+	struct semid_ds *semstat; 
+	unsigned short *array; 
+};
+
+
+int Inicializar_Semaforo(int semid, unsigned short *array){
+	union semun arg;
+	arg.array = array;
+	if(semctl (semid, sizeof(array)/sizeof(array[0]), SETALL, arg) == -1){
+      return ERROR;
+   }
+   return OK;
+}
+
+int Borrar_Semaforo(int semid){
+   if(semctl(semid, 0, IPC_RMID) == -1){
+      return ERROR;
+   }
+   return OK;
+}
+
+int Crear_Semaforo(key_t key, int size, int *semid){
+   int ret = 0;
+   int id = semget(key, size, IPC_CREAT | IPC_EXCL | SHM_R | SHM_W);
+   if(id == -1 && errno == EEXIST){
+      id = semget(key, size, SHM_R | SHM_W);
+      ret = 1;
+      if(id == -1){
+         return -1;
+      }
+   }
+   *semid = id;
+   return ret;
+}
+
+int Down_Semaforo(int id, int num_sem, int undo){
+   struct sembuf down;
+   down.sem_num = num_sem;
+   down.sem_op = -1;
+   if(undo){
+      down.sem_flg = SEM_UNDO;
+   }
+   if(semop(id, &down, 1) == -1){
+      return ERROR;
+   }
+   return OK;
+}
+
+int DownMultiple_Semaforo(int id,int size,int undo,int *active){
+   int i;
+   for(i = 0; i <  size; i++){
+      if(Down_Semaforo(id, active[i], undo) == ERROR){
+         return ERROR;
+      }
+   }
+   return OK;
+}
+
+int Up_Semaforo(int id, int num_sem, int undo){
+   struct sembuf up;
+   up.sem_num = num_sem;
+   up.sem_op = 1;
+   if(undo){
+      up.sem_flg = SEM_UNDO;
+   }
+   if(semop(id, &up, 1) == -1){
+      return ERROR;
+   }
+   return OK;
+}
+
+int UpMultiple_Semaforo(int id,int size, int undo, int *active){
+   int i;
+   for(i = 0; i <  size; i++){
+      if(Up_Semaforo(id, active[i], undo) == ERROR){
+         return ERROR;
+      }
+   }
+   return OK;
+}
