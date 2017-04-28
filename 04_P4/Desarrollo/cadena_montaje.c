ddd884e13b097e47f08d95aebc58b97a01ae347d
blancams <blancamartinselgas@gmail.com>
2017-04-28 17:13:15 +0200

    Cadena montaje

--- 04_P4/Desarrollo/cadena_montaje.c
+++ 04_P4/Desarrollo/cadena_montaje.c
@@ -12,7 +12,7 @@
 #include <ctype.h>
 
 #define NUM_PROC 3
-#define MAX_CHAR 80	
+#define MAX_CHAR 4000
 
 #define N_KEY 300
 #define F_KEY "/bin/cat"
@@ -41,7 +41,7 @@ int main(int argc, char *argv[]){
       exit(EXIT_FAILURE);
    }
 
-   msqid = msgget(clave, IPC_CREAT | IPC_EXCL | 0660/*| S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH*/);
+   msqid = msgget(clave, IPC_CREAT | IPC_EXCL | 0660);
    if(msqid == -1){
       printf("Error al crear la cola de mensajes. %s\n", strerror(errno));
       exit(EXIT_FAILURE);
@@ -63,17 +63,28 @@ int main(int argc, char *argv[]){
          printf("Error al abrir el fichero de lectura.\n");
          exit(EXIT_FAILURE);
       }
-      fread(msg.mtext, sizeof(char), MAX_CHAR, f1);
-      msg.tipo = 1;
-      msqid = msgget(clave, 0660);
-      if(msqid == -1){
-         printf("Error al acceder a la cola de mensajes. %s\n", strerror(errno));
-         exit(EXIT_FAILURE);
+      while(!feof(f1)){
+         if(fread(msg.mtext, sizeof(char), MAX_CHAR, f1) < MAX_CHAR) break;
+         msg.tipo = 1;
+         msqid = msgget(clave, 0660);
+         if(msqid == -1){
+            printf("Error al acceder a la cola de mensajes. %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+         }
+         sleep(1);
+         if (msgsnd(msqid, &msg, sizeof(mensaje) - sizeof(long), 0) == -1){
+            printf("%s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+         }
       }
-      if (msgsnd(msqid, &msg, sizeof(char)*strlen(msg.mtext), IPC_NOWAIT) == -1){
+      msg.tipo = 1;
+      strcpy(msg.mtext, "FIN");
+      sleep(1);
+      if (msgsnd(msqid, &msg, sizeof(mensaje) - sizeof(long), 0) == -1){
          printf("%s\n", strerror(errno));
          exit(EXIT_FAILURE);
       }
+      printf("1HB");
       fclose(f1);
    } else if(i == 2){
       int cnt;
@@ -82,18 +93,33 @@ int main(int argc, char *argv[]){
          printf("Error al acceder a la cola de mensajes. %s\n", strerror(errno));
          exit(EXIT_FAILURE);
       }
-      if(msgrcv(msqid, &msg, sizeof(mensaje) - sizeof(long), 1,  MSG_NOERROR) == -1){
-         printf("%s\n", strerror(errno));
-         exit(EXIT_FAILURE);      
-      }
-      while(cnt < strlen(msg.mtext)){
-         msg.mtext[cnt] = (char) toupper(msg.mtext[cnt]);
-         cnt++;
+     // strcpy(msg.mtext, "jhbs");
+      while(1){
+         if(msgrcv(msqid, &msg, sizeof(mensaje) - sizeof(long), 1,  0) == -1){
+            printf("%s\n", strerror(errno));
+            exit(EXIT_FAILURE);      
+         }
+         //printf("\n%s", msg.mtext);
+         if(!strcmp(msg.mtext, "FIN")){
+            printf("2HB");
+            break;
+         } 
+         //printf("%d\n", i);
+         cnt = 0;
+         while(cnt < strlen(msg.mtext)){
+            msg.mtext[cnt] = (char) toupper(msg.mtext[cnt]);
+            cnt++;
+         }
+         msg.tipo = 2;
+         if(msgsnd(msqid, (struct msgbuf *) &msg, sizeof(mensaje) - sizeof(long), 0) == -1){
+            printf("%s\n", strerror(errno));
+            exit(EXIT_FAILURE); 
+         }
       }
       msg.tipo = 2;
-      if(msgsnd(msqid, (struct msgbuf *) &msg, sizeof(mensaje) - sizeof(long), IPC_NOWAIT) == -1){
+      if (msgsnd(msqid, &msg, sizeof(char)*strlen(msg.mtext), 0) == -1){
          printf("%s\n", strerror(errno));
-         exit(EXIT_FAILURE); 
+         exit(EXIT_FAILURE);
       }
    } else if(i == 3){
       FILE *f2 = fopen(argv[2], "w+");
@@ -106,11 +132,23 @@ int main(int argc, char *argv[]){
          printf("Error al acceder a la cola de mensajes. %s\n", strerror(errno));
          exit(EXIT_FAILURE);
       }
-      if(msgrcv(msqid, (struct msgbuf *) &msg, sizeof(mensaje) - sizeof(long), 2,  MSG_NOERROR) == -1){
-         printf("%s\n", strerror(errno));
-         exit(EXIT_FAILURE); 
+      while(1){
+         if(msgrcv(msqid, (struct msgbuf *) &msg, sizeof(mensaje) - sizeof(long), 2,  MSG_NOERROR) == -1){
+            printf("%s\n", strerror(errno));
+            exit(EXIT_FAILURE); 
+         }
+         //printf("\nREC");
+         //printf("\n\n%s%lu", msg.mtext, strlen(msg.mtext));
+         if(!strcmp(msg.mtext, "FIN")){
+            printf("\n3HB");
+            break;
+         }
+         //printf("\nCOMP");
+         if(sizeof(msg.mtext) < MAX_CHAR) break;
+         fwrite(msg.mtext, sizeof(char), MAX_CHAR, f2);
+         //printf("\nESC");
       }
-      fwrite(msg.mtext, sizeof(char), MAX_CHAR, f2);
+      printf("%d\n", i);
       fclose(f2);
       msgctl(msqid, IPC_RMID, (struct msqid_ds *) NULL);
    } else{
